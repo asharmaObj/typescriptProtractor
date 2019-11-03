8a9f33fe044a1ee345d67d83f1321502037c01a5
maxibawa@gmail.com <maxibawa@gmail.com>
2019-11-03 19:06:03 +1100

    test

--- conf.ts
+++ conf.ts
@@ -7,6 +7,12 @@
 // description hints.
 //
 // To run this example, run `protractor conf.js`.
+import {browser} from 'protractor';
+
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+var jasmineReporters = require('jasmine-reporters');
+var dashboardReportDirectory = 'reports';
+
 exports.config = {
   framework: 'jasmine',
   capabilities: {
@@ -20,5 +26,71 @@ exports.config = {
 
   // You could set no globals to true to avoid jQuery '$' and protractor '$'
   // collisions on the global namespace.
-  noGlobals: true
+  noGlobals: true,
+
+  
+  //non angular app
+  //browser.waitForAngularEnabled(false)
+
+  // before all test suites
+  onPrepare: function () {
+    browser.driver.manage().window().maximize();
+
+    // adding a wait before test cases.
+    browser.manage().timeouts().implicitlyWait(5000);
+
+      // xml report generated for dashboard
+      jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+          consolidateAll: true,
+          savePath: dashboardReportDirectory + '/xml',
+          filePrefix: 'xmlOutput'
+      }));
+
+      var fs = require('fs-extra');
+      if (!fs.existsSync(dashboardReportDirectory)) {
+          fs.mkdirSync(dashboardReportDirectory);
+      }
+
+      jasmine.getEnv().addReporter({
+          specDone: function (result) {
+              if (result.status == 'failed') {
+                  browser.getCapabilities().then(function (caps) {
+                      var browserName = caps.get('browserName');
+
+                      browser.takeScreenshot().then(function (png) {
+                          var stream = fs.createWriteStream(dashboardReportDirectory + '/' + browserName + '-' + result.fullName + '.png');
+                          stream.write(new Buffer(png, 'base64'));
+                          stream.end();
+                      });
+                  });
+              }
+          }
+      });
+
+  },
+
+  onComplete: function () {
+      var browserName, browserVersion;
+      var capsPromise = browser.getCapabilities();
+
+      capsPromise.then(function (caps) {
+          browserName = caps.get('browserName');
+          browserVersion = caps.get('version');
+          var platform = caps.get('platform');
+
+          var HTMLReport = require('protractor-html-reporter-2');
+        var  testConfig = {
+              reportTitle: 'Protractor Test Execution Report',
+              outputPath: dashboardReportDirectory,
+              outputFilename: 'index',
+              screenshotPath: './',
+              testBrowser: browserName,
+              browserVersion: browserVersion,
+              modifiedSuiteName: false,
+              screenshotsOnlyOnFailure: true,
+              testPlatform: platform
+          };
+          new HTMLReport().from(dashboardReportDirectory + '/xml/xmlOutput.xml', testConfig);
+      });
+  },
 };
